e7dc17c31fed6bb0ddd44fb2db62e650edffcea9
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2020-02-23 20:49:18 +0000

    Blazor Demo Youtube

---
+++ Model/PomodoroSetttings.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoYoutube.Model
+{
+    public class PomodoroSetttings
+    {
+
+        [Required(ErrorMessage = "Specify the Pomodoro duration time")]
+        [Range(1, 60, ErrorMessage = "Pomodoro duration time invalid. The value must be between 1 and 60.")]
+        public int PomodoroDuration { get; set; }
+
+
+        [Range(1, 15, ErrorMessage = "Short break time invalid. The value must be between 1 and 15.")]
+        public int ShortBreak { get; set; }
+
+
+        [Range(15, 60, ErrorMessage = "Short break time invalid. The value must be between 15 and 60.")]
+        public int LongBreak { get; set; }
+    }
+
+    public enum TimerType
+    {
+        Pomodro = 1,
+        ShortBreak = 2,
+        LongBreak = 3
+    }
+}
